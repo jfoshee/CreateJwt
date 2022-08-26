dc6c0148211b182d644799cab8276e3f8da564b7
Jacob Foshee <jacobf@gmail.com>
2022-08-26 14:52:53 -0500

    Create JWT in code using approach described in blog below
    Generating this JWT for example:
    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkphY29iRm9zaGVlTXlJc3N1ZXIiLCJuYW1laWQiOiJKYWNvYkZvc2hlZU15SXNzdWVyIiwibmJmIjoxNjYxNTQzNDY5LCJleHAiOjE2NjIxNDgyNjksImlhdCI6MTY2MTU0MzQ2OSwiaXNzIjoiaHR0cDovL215c2l0ZS5jb20iLCJhdWQiOiJodHRwOi8vbXlhdWRpZW5jZS5jb20ifQ.RNEsgOcV0IgFva9ndW9XaqxwXoVMJUrPCTbP-FRAqZU
    
    - [Creating And Validating JWT Tokens In C# .NET - .NET Core Tutorials](https://dotnetcoretutorials.com/2020/01/15/creating-and-validating-jwt-tokens-in-asp-net-core/)
    - [.NET 6.0 - JWT Authentication Tutorial with Example API | Jason Watmore's Blog](https://jasonwatmore.com/post/2021/12/14/net-6-jwt-authentication-tutorial-with-example-api)
    - [dotnet-6-jwt-authentication-api/UserService.cs at master · cornflourblue/dotnet-6-jwt-authentication-api](https://github.com/cornflourblue/dotnet-6-jwt-authentication-api/blob/master/Services/UserService.cs)
    - [JwtSecurityTokenHandler.CreateToken(SecurityTokenDescriptor) Method (System.IdentityModel.Tokens.Jwt) - Azure for .NET Developers | Microsoft Docs](https://docs.microsoft.com/en-us/dotnet/api/system.identitymodel.tokens.jwt.jwtsecuritytokenhandler.createtoken?view=azure-dotnet#system-identitymodel-tokens-jwt-jwtsecuritytokenhandler-createtoken(microsoft-identitymodel-tokens-securitytokendescriptor))
    - [Create development-time JWTs using dotnet user-jwts](https://craftbakery.dev/create-jwt-using-dotnet-user-jwts/)
    - [localhost:5245/jwt?userId=JacobFosheeMyIssuer](http://localhost:5245/jwt?userId=JacobFosheeMyIssuer)
    - [jwt.ms: Welcome!](https://jwt.ms/)
    - [c# - Bearer error - invalid_token - The signature key was not found - Stack Overflow](https://stackoverflow.com/questions/58563661/bearer-error-invalid-token-the-signature-key-was-not-found)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+	var mySecret = "asdv234234^&%&^%&^hjsdfb2%%%";
+	var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthorization();
-builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(options => {
+    options.TokenValidationParameters.IssuerSigningKey = mySecurityKey;
+});
 
 var app = builder.Build();
 app.UseAuthorization();
 
 app.MapGet("/", (ClaimsPrincipal user) => $"Hello User: {user?.Identity?.Name}");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello User: {user?.Identity?.Name}").RequireAuthorization();
+app.MapGet("/jwt", (string userId) => GenerateToken(userId));
 
 app.Run();
+
+
+string GenerateToken(string userId)
+{
+	// var mySecret = "asdv234234^&%&^%&^hjsdfb2%%%";
+	// var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
+	var myIssuer = "http://mysite.com";
+	var myAudience = "http://myaudience.com";
+
+	var tokenHandler = new JwtSecurityTokenHandler();
+	var tokenDescriptor = new SecurityTokenDescriptor
+	{
+		Subject = new ClaimsIdentity(new Claim[]
+		{
+			new Claim(ClaimTypes.Name, userId),
+			new Claim(ClaimTypes.NameIdentifier, userId),
+		}),
+		Expires = DateTime.UtcNow.AddDays(7),
+		Issuer = myIssuer,
+		Audience = myAudience,
+		SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
+	};
+
+	var token = tokenHandler.CreateToken(tokenDescriptor);
+	return tokenHandler.WriteToken(token);
+}
